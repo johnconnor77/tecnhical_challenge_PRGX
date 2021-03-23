330d2b704ce51603b65d8f6bae1b1da0ea3127b2
johnconnor <juan.calle@ubidots.com>
2021-03-23 15:44:37 -0500

    Execution permissions and correct JSON response implementend

--- sum_api_prgx.py
+++ sum_api_prgx.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+
 from sum_inout import SumModel
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 import uvicorn
 import logging
 
@@ -16,12 +19,16 @@ app = FastAPI(title="PRGX SUM API",
           description="Allows the addition of two certain digits given by User")
 async def sum_input(sum_in: SumModel):
     """
+    Retrieves the addition of two certain digits given by User request body
+        :param sum_in: Class that represent the correct structure for post request body
+
+        :return: JSONResponse of the addition of digits otherwise the given error response
     """
     x = sum_in.x
     y = sum_in.y
     sum_out = x + y
 
-    return {"sum": sum_out}
+    return JSONResponse({"sum": sum_out})
 
 
 if __name__ == "__main__":
